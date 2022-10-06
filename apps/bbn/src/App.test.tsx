cf1b7f5308548a46522ae7aa3a01db885365c214
“Denis <denisnv@gmail.com>
2022-10-06 10:14:24 +0100

    updated bbn to react 18

--- apps/bbn/src/App.test.tsx
+++ apps/bbn/src/App.test.tsx
@@ -1,14 +1,9 @@
 import React from 'react';
 import { render } from '@testing-library/react';
-import { MemoryRouter } from 'react-router-dom';
 import App from './App';
 
 test('renders learn react link', () => {
-  const { getByText } = render(
-    <MemoryRouter>
-      <App />
-    </MemoryRouter>
-  );
+  const { getByText } = render(<App />);
   const linkElement = getByText(/welcome to razzles/i);
   expect(linkElement).toBeDefined();
 });
--- apps/bbn/src/client.tsx
+++ apps/bbn/src/client.tsx
@@ -1,15 +1,9 @@
 import React from 'react';
-import { render } from 'react-dom';
-import { BrowserRouter } from 'react-router-dom';
+import { createRoot } from 'react-dom/client';
 
 import App from './App';
 
-render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>,
-  document.getElementById('root')
-);
+createRoot(document.getElementById('root')).render(<App />);
 
 if (module.hot) {
   module.hot.accept();
