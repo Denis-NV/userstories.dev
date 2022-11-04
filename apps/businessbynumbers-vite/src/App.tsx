65ebbcb57d73151fe9d325d90d55789bab9ad136
“Denis <denisnv@gmail.com>
2022-11-04 09:09:07 +0000

    husky test

--- apps/businessbynumbers-vite/src/App.tsx
+++ apps/businessbynumbers-vite/src/App.tsx
@@ -16,6 +16,7 @@ function App() {
         </a>
       </div>
       <h1>Vite + React</h1>
+      Business by numbers
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>count is {count}</button>
         <p>
