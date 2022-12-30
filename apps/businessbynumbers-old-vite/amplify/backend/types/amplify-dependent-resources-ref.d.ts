47adbf8fc71f3940afe832cb3111d21b2353c511
DenisNV <denisnv@gmail.com>
2022-12-30 10:00:54 +0000

    got rid of old bbn-vite

--- apps/businessbynumbers-old-vite/amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++
@@ -1,9 +0,0 @@
-export type AmplifyDependentResourcesAttributes = {
-    "api": {
-        "businessbynumbers": {
-            "GraphQLAPIKeyOutput": "string",
-            "GraphQLAPIIdOutput": "string",
-            "GraphQLAPIEndpointOutput": "string"
-        }
-    }
-}
