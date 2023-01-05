b29d3a88d7773b48c2cdd15e5499b47945e44d02
DenisNV <denisnv@gmail.com>
2023-01-05 10:39:01 +0000

    restructured bbn folders

--- apps/businessbynumbers/src/main.tsx
+++ apps/businessbynumbers/src/main.tsx
@@ -1,9 +1,9 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 
-import Auth0ProviderWithHistory from './config/Auth0ProviderWithHistory'
-import ApolloProvider from './config/ApolloProvider'
-import App from './App'
+import Auth0ProviderWithHistory from '@config/Auth0ProviderWithHistory'
+import ApolloProvider from '@config/ApolloProvider'
+import App from '@components/App'
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
