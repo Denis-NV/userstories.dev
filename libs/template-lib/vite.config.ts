1d88c2408629f468b6da76273d3b67a0b959e95c
DenisNV <denisnv@gmail.com>
2023-11-27 07:27:33 +0000

    added basic file structure for template-lib project

---
+++ libs/template-lib/vite.config.ts
@@ -0,0 +1,33 @@
+import react from '@vitejs/plugin-react'
+import path from 'node:path'
+import { defineConfig } from 'vite'
+import dts from 'vite-plugin-dts'
+
+import * as packageJson from './package.json'
+
+export default defineConfig({
+  plugins: [
+    react(),
+    dts({
+      insertTypesEntry: true,
+    }),
+  ],
+  build: {
+    lib: {
+      entry: path.resolve(__dirname, 'src/index.ts'),
+      name: '@ustrs/template-lib',
+      formats: ['es', 'umd'],
+      fileName: (format) => `lib.${format}.js`,
+    },
+    rollupOptions: {
+      external: [...Object.keys(packageJson.peerDependencies)],
+      output: {
+        globals: {
+          react: 'React',
+          'react-dom': 'ReactDOM',
+          'styled-components': 'styled',
+        },
+      },
+    },
+  },
+})
