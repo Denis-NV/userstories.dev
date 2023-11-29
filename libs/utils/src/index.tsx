54606d511f31d3393e6037872a7d5ed797310fa8
DenisNV <denisnv@gmail.com>
2023-11-29 07:10:12 +0000

    got rid of the utils package

--- libs/utils/src/index.tsx
+++
@@ -1,6 +0,0 @@
-export const pxToInt = (pxVal: string): number => parseInt(pxVal?.substring(0, pxVal.length - 2))
-
-export { default as Button } from './components/Button'
-export { default as Svg } from './components/Svg'
-
-export type { DeepOmit } from './types'
