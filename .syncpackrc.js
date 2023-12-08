622a4025333bdde041cb2e0e4c1be1e8d0a3d5a8
DenisNV <denisnv@gmail.com>
2023-12-08 06:49:36 +0000

    added a proper syncpack config

---
+++ .syncpackrc.js
@@ -0,0 +1,45 @@
+// @ts-check
+
+/** @type {import("syncpack").RcFile} */
+const config = {
+  semverGroups: [
+    {
+      label: 'use caret ranges',
+      packages: ['**'],
+      dependencyTypes: ['prod', 'dev', 'peer'],
+      dependencies: ['**'],
+      range: '^',
+    },
+  ],
+  versionGroups: [
+    {
+      label: 'Use workspace protocol when developing local packages',
+      dependencies: ['$LOCAL'],
+      dependencyTypes: ['prod', 'dev'],
+      pinVersion: 'workspace:*',
+    },
+    // {
+    //   label: 'Ensure some packages are ignored',
+    //   dependencies: ['**'],
+    //   packages: ['@ustrs/template-lib'],
+    //   dependencyTypes: ['prod', 'dev', 'peer'],
+    //   isIgnored: true,
+    // },
+    // {
+    //   label: 'Ensure semver are within the same range for specific packages',
+    //   dependencies: ['**'],
+    //   packages: ['@ustrs/template-lib'],
+    //   dependencyTypes: ['prod', 'dev', 'peer'],
+    //   policy: 'sameRange',
+    // },
+    {
+      label: 'Ensure semver are within the same range for all (remaining) packages',
+      dependencies: ['**'],
+      packages: ['**'],
+      dependencyTypes: ['prod', 'dev', 'peer'],
+      policy: 'sameRange',
+    },
+  ],
+}
+
+module.exports = config
