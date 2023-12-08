59a95cfa6a10298bcd33458df732910433875c6f
DenisNV <denisnv@gmail.com>
2023-12-08 07:55:26 +0000

    removed vite-plugin-linter dep

--- libs/react-components/vite.config.ts
+++ libs/react-components/vite.config.ts
@@ -3,23 +3,16 @@ import { resolve } from 'path'
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import dts from 'vite-plugin-dts'
-// import EsLint from 'vite-plugin-linter'
 
 import tsConfigPaths from 'vite-tsconfig-paths'
 
 import * as packageJson from './package.json'
-
-// const { EsLinter, linterPlugin } = EsLint
 
 // https://vitejs.dev/config/
 export default defineConfig(() => ({
   plugins: [
     react(),
     tsConfigPaths(),
-    // linterPlugin({
-    //   include: ['./src}/**/*.{ts,tsx}'],
-    //   linters: [new EsLinter({ configEnv })],
-    // }),
     dts({
       include: ['src/'],
     }),
@@ -32,10 +25,7 @@ export default defineConfig(() => ({
       fileName: (format) => `lib.${format}.js`,
     },
     rollupOptions: {
-      external: [
-        ...Object.keys(packageJson.peerDependencies),
-        ...Object.keys(packageJson.devDependencies),
-      ],
+      external: [...Object.keys(packageJson.peerDependencies)],
       output: {
         // https://rollupjs.org/configuration-options/#output-globals
         globals: {
