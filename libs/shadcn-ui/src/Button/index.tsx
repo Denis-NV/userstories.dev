d4384d2a35cf11978ab9bdf3c1ccd4c6870fec41
DenisNV <denisnv@gmail.com>
2023-12-19 07:31:52 +0000

    enadled and implemented tailwind css in shadcn-ui package

--- libs/shadcn-ui/src/Button/index.tsx
+++ libs/shadcn-ui/src/Button/index.tsx
@@ -4,7 +4,21 @@ type TProps = {
 }
 
 const Button = ({ onClick, children }: TProps) => {
-  return <button onClick={onClick}>{children}</button>
+  return (
+    <button
+      onClick={onClick}
+      className="
+      rounded-md 
+      bg-slate-500 
+      p-2 
+      font-bold 
+      text-white 
+      hover:bg-slate-600 
+      active:bg-slate-700"
+    >
+      {children}
+    </button>
+  )
 }
 
 export default Button
