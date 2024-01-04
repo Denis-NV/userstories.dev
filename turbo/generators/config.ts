376236ec2d088477bb85bedf8b3407def7b6dfbe
DenisNV <denisnv@gmail.com>
2024-01-04 07:28:51 +0000

    almost implemented vite lib storybook generator

--- turbo/generators/config.ts
+++ turbo/generators/config.ts
@@ -1,12 +1,12 @@
 import { PlopTypes } from '@turbo/gen'
 import { getScope } from './utils'
+import { updateJson } from './utils/json'
 
 // Promts docs: https://github.com/SBoudrias/Inquirer.js/#question
 
 export default function generator(plop: PlopTypes.NodePlopAPI): void {
-  plop.setGenerator('vitelib', {
-    description:
-      'Vite based library package that exports transpiled umd and cjs files and can be optionally published on NPM',
+  plop.setGenerator('vite-lib', {
+    description: 'Buildable Vite library',
     prompts: [
       {
         type: 'input',
@@ -39,6 +39,71 @@ export default function generator(plop: PlopTypes.NodePlopAPI): void {
         destination: '{{ turbo.paths.root }}/libs/{{ name }}',
         base: `templates/vitelib`,
         templateFiles: `templates/vitelib/**/*`,
+      },
+    ],
+  })
+
+  plop.setGenerator('vite-lib-storybook', {
+    description: 'Buildable Vite library with Storybook',
+    prompts: [
+      {
+        type: 'input',
+        name: 'name',
+        message: 'Library name:',
+        validate: (input: string) => {
+          if (input.includes('.')) {
+            return 'name cannot include an extension'
+          }
+          if (input.includes(' ')) {
+            return 'name cannot include spaces'
+          }
+          if (!input) {
+            return 'name is required'
+          }
+          return true
+        },
+      },
+      {
+        type: 'input',
+        name: 'author',
+        message: 'Author',
+        default: 'Denis Nemytov',
+      },
+    ],
+    actions: [
+      getScope,
+      // {
+      //   type: 'addMany',
+      //   destination: '{{ turbo.paths.root }}/libs/{{ name }}',
+      //   base: `templates/vitelib`,
+      //   templateFiles: `templates/vitelib/**/*`,
+      // },
+      // {
+      //   type: 'addMany',
+      //   destination: '{{ turbo.paths.root }}/libs/{{ name }}',
+      //   base: `templates/vitelibstorybook`,
+      //   templateFiles: `templates/vitelibstorybook/**/*`,
+      // },
+      function appendJSON(): Promise<string> | string {
+        // updateJson(`libs/${answers.name}/package.json`, (pkgJson) => {
+        updateJson(`./package.json`, (pkgJson) => {
+          // if scripts is undefined, set it to an empty object
+          pkgJson.scripts = pkgJson.scripts ?? {}
+          pkgJson.scripts.storybook = 'storybook dev -p 6006'
+          pkgJson.scripts['build-storybook'] = 'storybook build'
+
+          pkgJson.devDependencies = pkgJson.devDependencies ?? {}
+          pkgJson.devDependencies['storybook'] = '^7.6.5'
+          pkgJson.devDependencies['@storybook/addon-essentials'] = '^7.6.5'
+          pkgJson.devDependencies['@storybook/addon-interactions'] = '^7.6.5'
+          pkgJson.devDependencies['@storybook/addon-links'] = '^7.6.5'
+          pkgJson.devDependencies['@storybook/react'] = '^7.6.5'
+          pkgJson.devDependencies['@storybook/react-vite'] = '^7.6.5'
+          // return modified JSON object
+          return pkgJson
+        })
+
+        return 'JSON Updated'
       },
     ],
   })
--- turbo/generators/utils/index.ts
+++ turbo/generators/utils/index.ts
@@ -1,33 +1,4 @@
-import { parse, printParseErrorCode } from 'jsonc-parser'
-import type { ParseError, ParseOptions } from 'jsonc-parser'
-import fs from 'fs'
-
-export function parseJson<T extends object = any>(
-  input: string,
-  options?: ParseOptions,
-): T | undefined {
-  try {
-    const data = fs.readFileSync(input, 'utf8')
-
-    try {
-      return JSON.parse(data)
-    } catch {
-      console.log('Error parsing JSON')
-    }
-
-    const errors: ParseError[] = []
-
-    const result: T = parse(data, errors, options)
-
-    if (errors.length > 0) {
-      throw new Error(printParseErrorCode(errors[0].error))
-    }
-
-    return result
-  } catch (err) {
-    console.error(err)
-  }
-}
+import { parseJson } from './json'
 
 export const getScope = (answers: any): string => {
   answers.scope = parseJson('package.json')?.name
