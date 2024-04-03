258534ad2196f5ee1048a36b05c1d0d5c8b0a604
DenisNV <denisnv@gmail.com>
2024-04-03 07:56:14 +0100

    added vitest rtl generator

--- turbo/generators/config.ts
+++ turbo/generators/config.ts
@@ -106,4 +106,57 @@ export default function generator(plop: PlopTypes.NodePlopAPI): void {
       },
     ],
   })
+
+  plop.setGenerator('add-vitest-rtl', {
+    description: 'Add Vitest-based and RTL testing setup to an existing Vite-based lib or app',
+    prompts: [
+      {
+        type: 'input',
+        name: 'packageName',
+        message: 'Package name:',
+        validate: (input: string) => {
+          if (input.includes('.')) {
+            return 'name cannot include an extension'
+          }
+          if (input.includes(' ')) {
+            return 'name cannot include spaces'
+          }
+          if (!input) {
+            return 'Package name is required'
+          }
+          return true
+        },
+      },
+    ],
+    actions: [
+      getScope,
+      {
+        type: 'addMany',
+        destination: '{{ turbo.paths.root }}/{{ packageName }}',
+        base: `templates/vitestrtl`,
+        templateFiles: `templates/vitestrtl/**/*`,
+      },
+      function appendJSON(answers: any): Promise<string> | string {
+        updateJson(`./${answers.name}/package.json`, (pkgJson) => {
+          // if scripts is undefined, set it to an empty object
+          pkgJson.scripts = pkgJson.scripts ?? {}
+          pkgJson.scripts.test = 'vitest run'
+          pkgJson.scripts['test:watch'] = 'vitest'
+
+          pkgJson.devDependencies = pkgJson.devDependencies ?? {}
+          pkgJson.devDependencies['vitest'] = '^1.3.1'
+          pkgJson.devDependencies['jsdom'] = '^24.0.0'
+          pkgJson.devDependencies['@testing-library/dom'] = '^9.3.4'
+          pkgJson.devDependencies['@testing-library/jest-dom'] = '^6.2.0'
+          pkgJson.devDependencies['@testing-library/react'] = '^14.1.2'
+          pkgJson.devDependencies['@testing-library/user-event'] = '^14.5.2'
+
+          // return modified JSON object
+          return pkgJson
+        })
+
+        return 'JSON Updated'
+      },
+    ],
+  })
 }
