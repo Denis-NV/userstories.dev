882aa4bea38c6764c013e356dea7eff370f03fd2
DenisNV <denisnv@gmail.com>
2024-09-21 09:03:31 +0100

    created custom combo box

--- apps/grainbygrain/src/components/CustomerSelect/index.tsx
+++ apps/grainbygrain/src/components/CustomerSelect/index.tsx
@@ -3,20 +3,15 @@ import { useQuery } from '@apollo/client'
 import { useAccessToken } from '@nhost/react'
 import { MagnifyingGlassIcon } from '@radix-ui/react-icons'
 import debounce from 'debounce'
+import { ChevronsUpDown } from 'lucide-react'
 
-import {
-  Select,
-  SelectContent,
-  SelectGroup,
-  SelectItem,
-  SelectLabel,
-  SelectTrigger,
-  SelectValue,
-} from '@/components/ui/select'
-import { Input } from '@/components/ui/input'
+import { cn } from '@/utils'
 import { Label } from '@/components/ui/label'
+import { Button } from '@/components/ui/button'
+import { Popover, PopoverContent, PopoverTrigger } from '@/components/ui/popover'
 
 import { CUSTOMERS_BY_DISTRICT_QUERY } from './gql'
+import SelectItem from './SelectItem'
 
 type TProps<T extends string> = {
   onChange: (value: T) => void
@@ -53,6 +48,12 @@ const CustomerSelect = <T extends string>({
   const noItemsFound = !districts
     ?.map((dist) => dist.customers?.length !== 0)
     .find((item) => item === true)
+  const customers = districts?.reduce(
+    (prev, cur) => {
+      return [...prev, ...cur.customers]
+    },
+    [] as (typeof districts)[0]['customers'],
+  )
 
   const handleSearch = useCallback(
     ({ target }: ChangeEvent<HTMLInputElement>) => setSearchStr(target?.value),
@@ -61,14 +62,9 @@ const CustomerSelect = <T extends string>({
 
   const handleOpen = useCallback(() => {
     setSearchStr('')
-  }, [setSearchStr])
+    setOpen((prev) => !prev)
+  }, [setSearchStr, setOpen])
 
-  const handleCloseClick = useCallback(() => {
-    setOpen(false)
-  }, [setOpen])
-  const handleOpenClick = useCallback(() => {
-    setOpen(true)
-  }, [setOpen])
   const handleValueChange = useCallback(
     (e: T) => {
       setOpen(false)
@@ -79,29 +75,39 @@ const CustomerSelect = <T extends string>({
   )
 
   return (
-    <Select onValueChange={handleValueChange} value={value} open={open} onOpenChange={handleOpen}>
+    <Popover open={open} onOpenChange={handleOpen} modal>
       <TriggerWrapperComp>
-        <SelectTrigger disabled={!districts} className={triggerClassName} onClick={handleOpenClick}>
-          <SelectValue placeholder={placeholder} />
-        </SelectTrigger>
+        <PopoverTrigger asChild>
+          <Button
+            variant="outline"
+            role="combobox"
+            aria-expanded={open}
+            className={cn('w-full justify-between', triggerClassName)}
+          >
+            <p className={cn(value && '... truncate')}>
+              {value && customers
+                ? customers?.find((customer) => customer.id === value)?.name
+                : placeholder}
+            </p>
+            <ChevronsUpDown className="h-4 w-4 shrink-0 opacity-50" />
+          </Button>
+        </PopoverTrigger>
       </TriggerWrapperComp>
-
-      <SelectContent onPointerDownOutside={handleCloseClick}>
-        <div className="flex items-center space-x-2 py-1 pl-2 pr-1">
+      <PopoverContent align="start" className="p-0">
+        <div className="flex items-center space-x-2 border-b-[1px] py-1 pl-2 pr-1">
           <MagnifyingGlassIcon className="h-4 w-4 opacity-50" />
           <Label htmlFor="search" className="sr-only">
             Search
           </Label>
-          <Input
-            id="search"
+          <input
+            type="search"
+            className="h-8 w-full bg-transparent px-3 py-1 text-sm transition-colors placeholder:text-slate-500 focus-visible:outline-none disabled:cursor-not-allowed disabled:opacity-50 dark:border-slate-800 dark:placeholder:text-slate-400 dark:focus-visible:ring-slate-300"
             placeholder="Search by name or address"
             onChange={debounce(handleSearch, 500)}
             onEmptied={debounce(handleSearch, 500)}
             onKeyDown={(e) => {
               e.stopPropagation()
             }}
-            type="search"
-            className="h-8"
           />
         </div>
 
@@ -110,22 +116,28 @@ const CustomerSelect = <T extends string>({
 
           {districts?.map(({ id: distId, name, customers }) => {
             return customers.length > 0 ? (
-              <SelectGroup key={distId}>
-                <SelectLabel>{name}</SelectLabel>
+              <div key={distId} role="group">
+                <div className="px-2 py-1.5 text-sm font-semibold">{name}</div>
 
                 {customers?.map(({ id: custId, name, address }) => (
-                  <SelectItem key={custId} value={custId} className="cursor-pointer">
+                  <SelectItem
+                    key={custId}
+                    value={custId}
+                    onValueChange={handleValueChange}
+                    isSelected={custId === value}
+                    className="cursor-pointer"
+                  >
                     <p className="... max-w-64 truncate">
                       {name} - {address}
                     </p>
                   </SelectItem>
                 ))}
-              </SelectGroup>
+              </div>
             ) : null
           })}
         </div>
-      </SelectContent>
-    </Select>
+      </PopoverContent>
+    </Popover>
   )
 }
 export default CustomerSelect
