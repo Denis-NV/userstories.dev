4ae2b1753883a0b3ed07cf0bfec43d42ac8665d6
DenisNV <denisnv@gmail.com>
2024-10-09 07:51:50 +0100

    added project flag

--- apps/grainbygrain/src/main.tsx
+++ apps/grainbygrain/src/main.tsx
@@ -27,7 +27,7 @@ import './styles.css'
 import { RouteParams, Routes } from './const'
 
 const pointToProd = false
-
+// trigger deploy
 const nhostClientParams: NhostReactClientConstructorParams =
   import.meta.env.DEV && !pointToProd
     ? {
