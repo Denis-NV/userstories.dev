88812feb915a871062ad5ecb312a6add59c6b866
DenisNV <denisnv@gmail.com>
2024-10-12 14:20:00 +0100

    marocchino/sticky-pull-request-comment@v2 for post deploy comment

--- apps/grainbygrain/src/main.tsx
+++ apps/grainbygrain/src/main.tsx
@@ -27,7 +27,7 @@ import './styles.css'
 import { RouteParams, Routes } from './const'
 
 const pointToProd = false
-
+//
 const nhostClientParams: NhostReactClientConstructorParams =
   import.meta.env.DEV && !pointToProd
     ? {
